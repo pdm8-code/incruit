97b32f3de7f0acd43ae2817eb4ef3c96b33f20ae
pdm8-code <pdmhb77@gmail.com>
2024-11-13 09:55:32 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,18 @@ $(document).ready(function(){
       },
   });
 
+  $('.btn-bar').click(function(){
+    $('.sitemap').addClass('active');
+  });
+
+  $('.close-btn').click(function(){
+    $('.sitemap').removeClass('active');
+  });
+
+  $('.intro-li').click(function(){
+    $(this).find('.intro-menu').toggleClass('active');
+  });
+
   //탑 버튼
   $('.fixed-bottom').click(function(){
     $('html,body').animate({scrollTop:0},100);
