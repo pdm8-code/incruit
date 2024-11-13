07006c7dc1d6762d59511cbba5564f45670a614d
pdm8-code <pdmhb77@gmail.com>
2024-11-13 14:20:04 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
-  //스와이퍼 슬라이드
-  var swiper = new Swiper('.companySwiper', {
 
+  // 사이트멥 클릭 이벤트
+
+  $(".header .container button.btn-bar").on("click", function(){
+      $(".sitemap").addClass("active");
+      $(".background-black").addClass("active");
+      $('html, body').css({'overflow': 'hidden', 'height': '100%'});
+      
+  });
+
+  $(".sitemap .close-btn").on("click", function(){
+      $(".sitemap").removeClass("active");
+      $(".background-black").removeClass("active");
+      $('html, body').css({'overflow': 'visible', 'height': 'none'});
+
+  });
+
+  $(".sitemap ul li a.intro").on("click", function(){
+      $(".sitemap ul li a.intro").toggleClass("active")
+      $(".sitemap ul .intro-li .intro-menu").toggleClass("active");
+  });
+
+  // sol-wrap 애니매이션 효과
+  $(".sol-wrap .container .sol-top .btn-package").addClass("active");
+  $(".sol-wrap .container .sol-top .btn-box button.btn-freeExper").addClass("active");
+  $(".sol-wrap .container .sol-top .btn-box button.btn-inquiry").addClass("active");
+
+  // 인크루트웍스 슬라이드
+  var swiper = new Swiper('.workSwiper', {
       direction: 'horizontal',
       loop: true,
-      speed: 800,
+      speed: 1000,
 
       navigation: {
           nextEl: '.swiper-button-next',
@@ -12,26 +38,17 @@ $(document).ready(function(){
       },
   });
 
-  $('.btn-bar').click(function(){
-    $('.sitemap').addClass('active');
-  });
-
-  $('.close-btn').click(function(){
-    $('.sitemap').removeClass('active');
-  });
-
-  $('.intro-li').click(function(){
-    $(this).find('.intro-menu').toggleClass('active');
+  //윈도우 리사이즈
+  $(window).resize(function(){
+    if($(this).width() > 768){
+      $('.sitemap').removeClass('active');
+      $(".background-black").removeClass("active");
+      $('html, body').css({'overflow': 'visible', 'height': 'none'});
+    }
   });
 
   //탑 버튼
   $('.top-btn').click(function(){
     $('html,body').animate({scrollTop:0},100);
-  });
-
-  $(window).resize(function(){
-    if($(this).width() > 768){
-      $('.sitemap').removeClass('active');
-    }
   });
 });
